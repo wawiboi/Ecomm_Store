c48734a9bc1430ba7caa61ea23e18380c958d823
Wawi Fernandez <wawifernandez@gmail.com>
2012-11-21 13:32:30 -0600

    Styled Products edit form in admin dashboard

--- GG_Model_Shop_Ver3/app/admin/products.rb
+++ GG_Model_Shop_Ver3/app/admin/products.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Product do
-  
+   form do |f|
+      f.inputs do
+        f.input :name
+        f.input :description
+        f.input :price
+        f.input :stock_quantity
+        f.input :image
+        f.input :category_id, :as => :select, :collection => Category.all
+      end
+      f.buttons
+   end
+   
+  show do
+    div do
+      simple_format product.description
+      image_tag product.image
+    end
+  end
 end
